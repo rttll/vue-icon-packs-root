126304e03883103e71001fb29e1420d044f5c359
Adro <acousens@gmail.com>
2021-06-20 22:30:14 -0700

    Add deps to packages

--- packages/vanilla/src/App.vue
+++ packages/vanilla/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     App.vue
-    <Functions />
-    <ArrowLoopLeft />
-    <ArrowsExpandOutline />
+    <!-- <Functions /> -->
+    <!-- <ArrowLoopLeft /> -->
+    <!-- <ArrowsExpandOutline /> -->
   </div>
 </template>
 
 <script>
-import { Functions } from 'vue-icon-packs/ri';
-import { ArrowLoopLeft } from 'vue-icon-packs/ti';
-import ArrowsExpandOutline from 'vue-icon-packs/dist/hi/sfc/ArrowsExpandOutline.vue';
+// import { Functions } from 'vue-icon-packs/ri';
+// import { ArrowLoopLeft } from 'vue-icon-packs/ti';
+// import ArrowsExpandOutline from 'vue-icon-packs/dist/hi/sfc/ArrowsExpandOutline.vue';
 
 export default {
   name: 'App',
-  components: { Functions, ArrowLoopLeft, ArrowsExpandOutline },
+  // components: { Functions, ArrowLoopLeft, ArrowsExpandOutline },
 };
 </script>
